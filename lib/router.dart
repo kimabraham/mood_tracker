0d44a69d8e1057c3b171306e6ce3b3a19473c709
kimabraham <kimkr88@icloud.com>
2024-07-07 23:45:09 +0900

    chage go_router version

--- lib/router.dart
+++ lib/router.dart
@@ -1,18 +1,31 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mood_tracker/common/main_navigation/main_navigation_screen.dart';
+import 'package:mood_tracker/features/auth/repositories/auth_repo.dart';
 import 'package:mood_tracker/features/auth/views/join_screen.dart';
 import 'package:mood_tracker/features/auth/views/login_screen.dart';
 
 final routerProvider = Provider(
   (ref) {
     return GoRouter(
+      redirect: (context, state) {
+        final isLoggedIn = ref.read(authRepoProvider).isLoggedIn;
+
+        if (!isLoggedIn) {
+          if (state.subloc != JoinScreen.routeUrl &&
+              state.subloc != LoginScreen.routeUrl) {
+            return JoinScreen.routeUrl;
+          }
+        }
+
+        return null;
+      },
       routes: [
         GoRoute(
           path: '/:tab(home|new|profile)',
           name: MainNavigationScreen.routeName,
           builder: (context, state) {
-            final tab = state.pathParameters['tab']!;
+            final tab = state.params['tab']!;
             return MainNavigationScreen(tab: tab);
           },
         ),
