90f1ba9e603ba2e3bb952d92c5ee302ea60842f0
kimabraham <kimkr88@icloud.com>
2024-07-08 18:55:41 +0900

    main navigation delete fab replace stack

--- lib/common/main_navigation/main_navigation_screen.dart
+++ lib/common/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mood_tracker/common/main_navigation/widgets/nav_tab.dart';
+import 'package:mood_tracker/constants/gaps.dart';
 import 'package:mood_tracker/constants/sizes.dart';
 import 'package:mood_tracker/features/posts/views/post_add_screen.dart';
 import 'package:mood_tracker/features/posts/views/posts_list_screen.dart';
@@ -46,8 +47,6 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           ),
         ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingAddButton(onTap: () => _onTap(1)),
       bottomNavigationBar: BottomAppBar(
         padding: EdgeInsets.zero,
         notchMargin: 5,
@@ -56,66 +55,59 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
         child: Container(
           decoration: BoxDecoration(
             border: Border(
-                top: BorderSide(
-              color: Colors.grey.shade200,
-              width: 1,
-            )),
-          ),
-          child: Row(
-            children: [
-              NavTab(
-                title: 'home',
-                isSelected: _selectedIndex == 0,
-                icon: FontAwesomeIcons.house,
-                onTap: () => _onTap(0),
-                selectedIndex: _selectedIndex,
+              top: BorderSide(
+                color: Colors.grey.shade200,
+                width: 1,
               ),
-              NavTab(
-                title: 'profile',
-                isSelected: _selectedIndex == 2,
-                icon: FontAwesomeIcons.solidUser,
-                onTap: () => _onTap(2),
-                selectedIndex: _selectedIndex,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class FloatingAddButton extends StatelessWidget {
-  final VoidCallback onTap;
-
-  const FloatingAddButton({
-    super.key,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: Sizes.size40),
-      child: SizedBox(
-        width: Sizes.size72,
-        height: Sizes.size72,
-        child: FloatingActionButton(
-          clipBehavior: Clip.antiAlias,
-          shape: const CircleBorder(
-            side: BorderSide(
-              width: Sizes.size1,
-              color: Color(0xff2555AA),
             ),
           ),
-          backgroundColor: const Color(0xff4B68B0),
-          onPressed: onTap,
-          elevation: 0,
-          highlightElevation: 0,
-          child: const FaIcon(
-            FontAwesomeIcons.plus,
-            color: Colors.white,
-            size: Sizes.size28,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Row(
+                children: [
+                  NavTab(
+                    title: 'home',
+                    isSelected: _selectedIndex == 0,
+                    icon: FontAwesomeIcons.house,
+                    onTap: () => _onTap(0),
+                    selectedIndex: _selectedIndex,
+                  ),
+                  Gaps.h96,
+                  NavTab(
+                    title: 'profile',
+                    isSelected: _selectedIndex == 2,
+                    icon: FontAwesomeIcons.solidUser,
+                    onTap: () => _onTap(2),
+                    selectedIndex: _selectedIndex,
+                  ),
+                ],
+              ),
+              Positioned(
+                // 가운데 플러스 버튼
+                left: (MediaQuery.of(context).size.width - Sizes.size72) / 2,
+                bottom: 0,
+                child: GestureDetector(
+                  // 버튼 전체 영역을 터치 가능하게 함
+                  onTap: () => _onTap(1), // 플러스 버튼을 누르면 새 게시물 화면으로 이동
+                  child: Container(
+                    // 버튼 스타일 및 레이아웃 설정
+                    width: Sizes.size72,
+                    height: Sizes.size72,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blue,
+                    ),
+                    alignment: Alignment.center, // 아이콘 가운데 정렬
+                    child: const FaIcon(
+                      FontAwesomeIcons.plus,
+                      color: Colors.white,
+                      size: Sizes.size28,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
